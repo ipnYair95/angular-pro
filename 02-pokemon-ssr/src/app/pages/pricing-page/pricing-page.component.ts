e0dc5f13bccb3696fa18a352dc9962ec31066aca
Yair Marin <edgar.marin@axity.com>
2025-10-13 16:11:24 -0600

    init project without ssr

---
+++ 02-pokemon-ssr/src/app/pages/pricing-page/pricing-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing-page',
+  imports: [],
+  templateUrl: './pricing-page.component.html',
+  styleUrl: './pricing-page.component.css',
+})
+export default class PricingPageComponent { }
